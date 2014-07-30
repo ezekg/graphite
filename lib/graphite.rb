a821ef2fcdc02cc3efc1d0fb27139bd1f2a777b7
Ezekiel Gabrielse <ezekg@yahoo.com>
2014-07-30 13:26:07 -0500

    add debug, fix legacy, add local killer

--- lib/graphite.rb
+++ lib/graphite.rb
@@ -5,19 +5,20 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('graphite', :path => extension_path)
 
 module Graphite
-    VERSION = "0.1.0"
-    DATE = "2014-07-21"
+    VERSION = "0.2.0"
+    DATE = "2014-07-30"
 end
 
 # Graphite : import fonts from font_dir into map
 # ----------------------------------------------------------------------------------------------------
 # @param font_dir [string] : path to top-level font directory
 # @param legacy_ie [bool] : support legacy versions of ie
+# @param debug [bool] : print debug information
 # ----------------------------------------------------------------------------------------------------
 # @return map of fonts, variable for each font
 
 module Sass::Script::Functions
-    def graphite(font_dir, legacy_ie = false)
+    def graphite(font_dir, legacy_ie, debug)
 
         def opts(value)
             value.options = options
@@ -38,17 +39,15 @@ module Sass::Script::Functions
             # valid styles
             valid_styles = ["normal", "italic"]
             # Get weights for font
-            valid_weights.each do |weight|
+            valid_weights.each do | weight |
                 # Check filename for weight
                 if font.include?(weight)
                     # Get styles for weight
                     styles = []
                     # Check filename for style
-                    valid_styles.each do |style|
-                        if font.include?(style)
-                            # Save to styles
-                            styles << style
-                        end
+                    valid_styles.each do | style |
+                        # Save to styles
+                        styles << style if font.include?(style)
                     end
                     # Keep hash of each weight
                     w = {
@@ -66,15 +65,15 @@ module Sass::Script::Functions
 
         # Add style to passed weight
         def set_font_style(font, style)
-            font.merge!(style) do |k, old, new|
+            font.merge!(style) do | k, o, n |
                 # Merge styles
-                if old.is_a?(Hash)
+                if o.is_a?(Hash)
                     # Get key => value of old map
-                    old.each do |key, value|
+                    o.each do | key, value |
                         # Does new map have key?
-                        if new.has_key?(key)
+                        if n.has_key?(key)
                             # Then concat values
-                            new.each do |k, v|
+                            n.each do | k, v |
                                 value.concat(v)
                             end
                         end
@@ -84,53 +83,68 @@ module Sass::Script::Functions
         end
 
         # Check if passed dir has fonts
-        def dir_has_fonts?(dir)
+        def dir_has_fonts?(dir, debug)
             # Keep hash of all fonts found
             fonts = {}
             # Valid font extensions
             valid_extensions = ["eot", "otf", "woff", "ttf", "svg"]
-            # Change dir to font_dir
-            Dir.chdir(dir) do
-                # Search each dir
-                Dir.glob("**/") do |d|
-                    # Check if dir has fonts
-                    Dir.chdir(d) do
-                        path = d
-                        # Clean up font name
-                        family = d.delete("/")
-                        # Get matched extensions
-                        extensions = []
-                        # Get matched weights and styles
-                        variations = {}
-                        # Check dir for matching extensions
-                        valid_extensions.each do |ext|
-                            Dir.glob("*.#{ext}") do |filename|
-                                if filename.size > 0
-                                    # Check if filename has weights
-                                    w = font_has_weights?(filename.to_s)
-                                    # Merge weights and styles
-                                    set_font_style(variations, w)
-                                    # Save extensions that exist
-                                    if extensions.include?(ext) === false
-                                        extensions << ext
+            # Check if directory exists
+            dir_name = File.dirname(dir)
+
+            if File.exists?(dir_name)
+                # Print debug
+                puts "Fonts directory found: #{dir}" if debug
+                # Change dir to font_dir
+                Dir.chdir(dir) do
+                    # Search each dir
+                    Dir.glob("**/") do | d |
+                        # Check if directory exists
+                        dir_name = File.dirname(d)
+
+                        if File.exists?(dir_name)
+                            # Check if dir has fonts
+                            Dir.chdir(d) do
+                                path = d
+                                # Clean up font name
+                                family = d.delete("/")
+                                # Print debug
+                                puts "Font family found: #{family}" if debug
+                                # Get matched extensions
+                                extensions = []
+                                # Get matched weights and styles
+                                variations = {}
+                                # Check dir for matching extensions
+                                valid_extensions.each do | ext |
+                                    Dir.glob("*.#{ext}") do | filename |
+                                        puts "Font found: #{filename}" if debug
+                                        # Check if filename has weights
+                                        w = font_has_weights?(filename.to_s)
+                                        # Merge weights and styles
+                                        set_font_style(variations, w)
+                                        # Save extensions that don't already exist
+                                        extensions << ext if extensions.include?(ext) === false
                                     end
                                 end
+                                # Build out font hash
+                                font = {
+                                    "#{family}" => {
+                                        "path" => path,
+                                        "extensions" => extensions,
+                                        "weights" => variations
+                                    }
+                                }
+                                # Merge into fonts hash
+                                fonts = fonts.merge(font)
+                                # Set Sass variable for font family
+                                set_sass_var(family)
                             end
+                        else
+                            puts "Directory was not found: #{dir}. Aborting mission."
                         end
-                        # Build out font hash
-                        font = {
-                            "#{family}" => {
-                                "path" => path,
-                                "extensions" => extensions,
-                                "weights" => variations
-                            }
-                        }
-                        # Merge into fonts hash
-                        fonts = fonts.merge(font)
-                        # Set Sass variable for font family
-                        set_sass_var(family)
                     end
                 end
+            else
+                puts "Directory was not found: #{dir}. Aborting mission."
             end
             # Return hash of all fonts
             return fonts
@@ -138,6 +152,12 @@ module Sass::Script::Functions
 
         # Assert types
         assert_type font_dir, :String, :font_dir
+        assert_type legacy_ie, :Bool, :legacy_ie
+        assert_type debug, :Bool, :debug
+
+        # Cast to bool
+        legacy_ie = legacy_ie.to_bool
+        debug = debug.to_bool
 
         # Define root path up to current working directory
         root = Dir.pwd
@@ -155,13 +175,13 @@ module Sass::Script::Functions
         # Create Sass variable for font path
         set_sass_var(font_dir, 'graphite_font_dir')
 
-        # Font naming convention : name-weight[-variant].extension
+        # Font naming convention : name-weight-variant.extension
         fonts = {
             "legacy-ie" => legacy_ie,
         }
 
         # Get all fonts in dir
-        f = dir_has_fonts?(dir_path)
+        f = dir_has_fonts?(dir_path, debug)
 
         # Merge results into fonts
         fonts = fonts.merge(f)
@@ -169,5 +189,5 @@ module Sass::Script::Functions
         # Convert hash to map, return
         Sass::Script::Value::Map.new(SassyHash[fonts])
     end
-    declare :Graphite, [:font_dir]
+    declare :graphite, [:font_dir]
 end
